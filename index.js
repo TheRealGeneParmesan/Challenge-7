0e7fc1752c4227957ae2361a9e4cf8825e71b315
TheRealGeneParmesan <Geneparmigiano@gmail.com>
2023-02-21 12:59:19 -0800

    added initial array questions, which will likely need to be changed

--- index.js
+++ index.js
@@ -2,17 +2,67 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [
 
-    
+// TODO: Create an array of questions for user input
+const questions = [{
+    type: 'input',
+    name: 'Title',
+    question: "What is the title of your project?"
+},
+
+{
+    type: 'input',
+    name: 'Description',
+    question: 'Provide a description of your project.'
+
+},
+
+{
+    type: 'input',
+    name: 'TableOfContents',
+    question: 'Click to be taken to the table of contents.'
+},
+
+{
+    type: 'input',
+    name: 'Installation',
+    question: 'Provide installation instructions for your project.'
+},
+
+{
+    type: 'input',
+    name: 'Usage',
+    question: 'Provide the usage information for your project.'
+},
+
+{
+    type: 'input',
+    name: 'Contributing',
+    question: 'Provide the contribution guidelines.'
+},
+
+{
+    type: 'input',
+    name: 'Tests',
+    question: 'We will figure out why we need tests.'
+},
+
+{
+    type: 'input',
+    name: 'Questions',
+    question: 'What is your GitHub username? ',
+    question: 'Enter your email address? '
+},
+
+
+
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
